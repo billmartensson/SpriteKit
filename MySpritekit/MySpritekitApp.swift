5ffcf97952986c3835a80b8837028306fdb7071c
Bill Mårtensson <bill@magictechnology.se>
2020-12-10 21:55:36 +0100

    Initial Commit

---
+++ MySpritekit/MySpritekitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySpritekitApp.swift
+//  MySpritekit
+//
+//  Created by Bill Martensson on 2020-12-10.
+//
+
+import SwiftUI
+
+@main
+struct MySpritekitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
